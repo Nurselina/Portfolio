75737a275db35707953256a995ccaaec60ba6f88
Nursel Özcan <ozcannnursel@gmail.com>
2024-05-11 01:10:53 +0300

    Certificate,Contact,Homepage,User pages created.

---
+++ src/main/java/com/project/portfolio/repository/homePage/HomePage.java
@@ -0,0 +1,26 @@
+package com.project.portfolio.repository.homePage;
+
+import com.project.portfolio.core.Base;
+import com.project.portfolio.repository.socialMedia.SocialMedia;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "HomePage")
+public class HomePage extends Base {
+
+    @Column(name = "detail")
+    private String detail;
+
+    @ManyToOne
+    @JoinColumn(name = "social_media_id")
+    private SocialMedia social_media;
+
+}
--- src/main/java/com/project/portfolio/repository/socialMedia/SocialMedia.java
+++ src/main/java/com/project/portfolio/repository/socialMedia/SocialMedia.java
@@ -1,23 +1,30 @@
 package com.project.portfolio.repository.socialMedia;
 
 import com.project.portfolio.core.Base;
+import com.project.portfolio.repository.homePage.HomePage;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name = "SocialMedias")
+@Table(name = "Social_Medias")
 
 public class SocialMedia extends Base {
 
     @Column(name = "link")
     private String link;
+
+    @OneToMany(mappedBy ="social_media")
+    private List<HomePage> homePageList;
 }
